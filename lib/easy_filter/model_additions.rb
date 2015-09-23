8b2534d33e2a76e4a73fe5727639c14ad0816bd2
kepes.peter <kepes.peter@codeplay.hu>
2015-09-23 22:21:32 +0200

    Correct Rubocop ABC errors with refactoring

--- lib/easy_filter/model_additions.rb
+++ lib/easy_filter/model_additions.rb
@@ -12,45 +12,70 @@ module EasyFilter
                                          exact: 'exact_',
                                          sort: 'sort',
                                          direction: 'direction' })
+      params = assign_defaults params, prefixes
+      create_filter params, prefixes
+    end
 
-      filter = self
-      params.each do |key, value|
-        next unless key.start_with?(prefixes[:main]) && !value.blank? && key != "#{prefixes[:main]}button"
-        filter = add_where(filter, del_prefix(key, prefixes[:main]), value, prefixes)
-      end
+    private
 
+    def assign_defaults(params, prefixes)
       params[prefixes[:sort]] ||= 'id'
       params[prefixes[:direction]] ||= 'desc'
+      params
+    end
+
+    def create_filter(params, prefixes)
+      filter = self
+      params.each do |key, value|
+        if key.start_with?(prefixes[:main]) && !value.blank? && key != "#{prefixes[:main]}button"
+          filter = add_where(filter, key, value, prefixes)
+        end
+      end
 
       # TODO: included model fields not in 'column_names'. Somehow need to check if given column name valid
       # sort_column = column_names.include?(params[prefixes[:sort]]) ? params[prefixes[:sort]] : add_model('id')
       filter.order("#{add_model params[prefixes[:sort]]} #{sort_direction(params, prefixes)}")
     end
 
-    private
-
     def sort_direction(params, prefixes)
       %w(asc desc).include?(params[prefixes[:direction]]) ? params[prefixes[:direction]] : 'desc'
     end
 
     def add_where(filter, field, value, prefixes)
-      if field.start_with?(prefixes[:from])
-        filter.where("#{add_model(del_prefix(field, prefixes[:from]))} >= ?", value)
+      f = prefixed? field, prefixes[:main]
+      return filter unless f
 
-      elsif field.start_with?(prefixes[:to])
-        filter.where("#{add_model(del_prefix(field, prefixes[:to]))} <= ?", value)
+      f, op = get_operator f, prefixes
+      f = add_model f
 
-      elsif field.start_with?(prefixes[:exact])
-        filter.where("#{add_model(del_prefix(field, prefixes[:exact]))} = ?", value)
-
+      if op.blank?
+        condition = "#{f} like ?"
+        value = "%#{value}%"
       else
-        filter.where("#{add_model(field)} like ?", "%#{value}%")
+        condition = "#{f} #{op}"
       end
+
+      filter.where(condition, value)
     end
 
-    def del_prefix(name, prefix)
-      return name.gsub(prefix, '').to_s if name.start_with?(prefix)
-      name
+    def prefixed?(text, prefix)
+      return false unless text.start_with? prefix
+      text.sub(prefix, '')
+    end
+
+    def get_operator(field, prefixes)
+      operators = {
+        prefixes[:from] => '>= ?',
+        prefixes[:to] => '<= ?',
+        prefixes[:exact] => '= ?'
+      }
+
+      operators.each do |k, v|
+        f = prefixed? field, k
+        return f, v if f
+      end
+
+      [field, nil]
     end
 
     def add_model(field)
--- lib/easy_filter/view_helpers.rb
+++ lib/easy_filter/view_helpers.rb
@@ -18,23 +18,7 @@ module EasyFilter
 
       filters.each do |filter|
         f = determine_column filter, model_class
-        form += render_easy 'form_field_open', prefixes
-
-        case f[:col_type]
-        when :datetime
-          form += render_easy 'field_datetime', prefixes, f
-
-        when :date
-          form += render_easy 'field_datetime', prefixes, f
-
-        when :array
-          form += render_easy 'field_array', prefixes, f
-
-        else
-          form += render_easy 'field_text', prefixes, f
-        end
-
-        form += render_easy 'form_field_close', prefixes
+        form += render_field f, prefixes
       end
 
       form += render_easy 'buttons', prefixes
@@ -43,26 +27,54 @@ module EasyFilter
 
     private
 
+    def render_field(filter, prefixes)
+      form = render_easy 'form_field_open', prefixes
+      view = column_view filter[:col_type]
+      form += render_easy view, prefixes, filter
+      form + render_easy('form_field_close', prefixes)
+    end
+
+    def column_view(col_type)
+      views = {
+        datetime: 'field_datetime',
+        date:     'field_datetime',
+        array:    'field_array'
+      }
+      view = views[col_type]
+      view ||= 'field_text'
+      view
+    end
+
     def determine_column(filter, model_class)
-      filter = { field: filter.to_s } if filter.is_a?(Symbol) || filter.is_a?(String)
-      filter[:field] = filter[:field].to_s
-      filter[:label] ||= t("activerecord.attributes.#{model_class.name.underscore}.#{filter[:field]}")
+      filter = check_filter_values filter, model_class
 
       if filter[:items].nil?
-        model_class.columns.each do |column|
-          if column.name == filter[:field]
-            filter[:col_type] = column.type
-            break
-          end
-        end
-        if filter[:col_type] == :boolean
-          filter[:items] = boolean_array
-          filter[:col_type] = :array
-        end
+        filter[:col_type] = column_type_from_model model_class, filter[:field]
+        filter = check_boolean filter
       else
         filter[:col_type] = :array
       end
 
+      filter
+    end
+
+    def check_boolean(filter)
+      if filter[:col_type] == :boolean
+        filter[:items] = boolean_array
+        filter[:col_type] = :array
+      end
+      filter
+    end
+
+    def column_type_from_model(model_class, field)
+      model_class.columns.each { |column| return column.type if column.name == field }
+      nil
+    end
+
+    def check_filter_values(filter, model_class)
+      filter = { field: filter.to_s } if filter.is_a?(Symbol) || filter.is_a?(String)
+      filter[:field] = filter[:field].to_s
+      filter[:label] ||= t("activerecord.attributes.#{model_class.name.underscore}.#{filter[:field]}")
       filter
     end
 
